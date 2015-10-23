af491b2e1fdfafd62a01b865135e60613e9b322e
David Loscutoff <dloscutoff@gmail.com>
2015-10-23 02:31:40 -0500

    First commit of Ouroboros.

---
+++ Ouroboros/README.md
@@ -0,0 +1,109 @@
+# Ouroboros
+
+An esoteric programming language wherein the code is a bunch of [snakes eating their own tails](http://en.wikipedia.org/wiki/Ouroboros).
+
+### How it works
+
+An Ouroboros program is a series of lines, each of which represents an independent ouroboros snake. Commands are single characters. The *head* is the first character of the line, and the *tail* is the last character. Each snake starts by not having swallowed any of its tail yet. The `(` and `)` commands make it swallow or regurgitate sections of its tail, thus changing the flow of execution.
+
+By way of illustration:
+
+    Abcdefghij  How the code looks (A is head, j is tail)
+
+    Abcd
+    j  e        How the code is treated, in its initial state
+    ihgf
+
+    jiAbc
+      h d       Code after swallowing two characters
+      gfe
+
+Control flow starts at the head and proceeds toward the tail. If it reaches the end of the snake or a section that has been eaten, it returns to the head. If the instruction pointer is ever swallowed, that snake dies (stops executing instructions). Regurgitating more than the snake has swallowed causes the tail to come back to its initial position. (It's an ouroboros, it has to have its tail in its mouth!)
+
+Each snake executes instructions in parallel with the others, with snakes from top to bottom each executing one instruction on each tick. Each snake has a stack, and there is also a shared stack that allows for inter-snake commerce. Empty stacks act as if they contain infinite zeroes (though the true length is accessible using L/l).
+
+### Commands
+
+Literals:
+
+    0-9 Push value (multiple digits work as a single multi-digit number)
+    a-f Push 10-15
+    ""  Push ASCII values of all characters between quotes, reversed for easy printing
+
+Stack operations:
+
+    \   Swap top two items on stack
+    @   Rotate top three items on stack
+    ;   Pop stack and discard
+    .   Duplicate top of stack
+    $   Toggle whether active stack is the shared stack or own stack (initially it's own stack)
+    s   Set own stack active
+    S   Set shared stack active
+    m   Pop own stack and push to shared stack
+    M   Pop shared stack and push to own stack
+    y   Push copy of top of own stack to shared stack
+    Y   Push copy of top of shared stack to own stack
+    l   Push length of own stack to active stack
+    L   Push length of shared stack to active stack
+
+Control flow operations:
+
+    (   Pop stack and eat that many instructions of tail
+    )   Pop stack and regurgitate that many instructions of tail
+    w   Pop stack and wait that many ticks before resuming execution
+
+Math:
+
+    +   Pop stack twice and add
+    -   Pop stack twice and subtract
+    *   Pop stack twice and multiply
+    /   Pop stack twice and divide
+    %   Pop stack twice and mod
+    _   Negate top of stack
+    I   Truncate top of stack to integer
+    =   Pop stack twice and push 1 if equal, 0 otherwise
+    <   Pop stack twice and push 1 if less than, 0 otherwise
+    >   Pop stack twice and push 1 if greater than, 0 otherwise
+    !   Logically negate top of stack
+    ?   Push a random number between 0 and 1
+
+Input/output:
+
+    n   Pop stack and output as a number
+    o   Pop stack and output as a character
+    r   Read next nonnegative integer from input and push (-1 on eof)
+    i   Read character from input and push (-1 on eof)
+
+### Example programs
+
+Print digits 0 through 9:
+
+    .n1+.9>(
+
+Hello world, straightforward version:
+
+    "Hello, World!"ooooooooooooo1(
+
+Hello world, shorter version using two snakes:
+
+    S"Hello, World!"1(
+    13wSoL!(
+
+Fibonacci sequence (endless):
+
+    1y(
+    S.@\.nao+
+
+[Collatz orbit](http://en.wikipedia.org/wiki/Collatz_conjecture) of input number:
+
+    rm1(
+    S.nao.2<22*(.2%.@@.3*1+@@*@2/\!*+
+
+Test whether input number is prime:
+
+    Sr0s1(
+    )S1+.@@.@@%!Ms+S.@@.@>6*(6s2=n1(
+
+Cat:
+
+    i.1+!2*(o
---
+++ Ouroboros/ouroboros.js
@@ -0,0 +1,312 @@
+// Define Stack class
+
+function Stack() {
+    this.stack = [];
+    this.length = 0;
+};
+
+Stack.prototype.push = function(item) {
+    this.stack.push(item);
+    this.length++;
+}
+
+Stack.prototype.pop = function() {
+    var result = 0;
+    if (this.length > 0) {
+        result = this.stack.pop();
+        this.length--;
+    }
+    return result;
+}
+
+Stack.prototype.top = function() {
+    var result = 0;
+    if (this.length > 0) {
+        result = this.stack[this.length - 1];
+    }
+    return result;
+}
+
+Stack.prototype.toString = function() {
+    return "" + this.stack;
+}
+
+// Define Snake class
+function Snake(code) {
+    this.code = code;
+    this.length = this.code.length;
+    this.ip = 0;
+    this.ownStack = new Stack();
+    this.currStack = this.ownStack;
+    this.alive = true;
+    this.wait = 0;
+    this.partialString = this.partialNumber = null;
+}
+
+Snake.prototype.step = function() {
+    if (!this.alive) {
+        return null;
+    }
+    if (this.wait > 0) {
+        this.wait--;
+        return null;
+    }
+    var instruction = this.code.charAt(this.ip);
+    var output = null;
+    console.log("Executing instruction " + instruction);
+    if (this.partialString !== null) {
+        // We're in the middle of a double-quoted string
+        if (instruction == '"') {
+            // Close the string and push its character codes in reverse order
+            for (var i = this.partialString.length - 1; i >= 0; i--) {
+                this.currStack.push(this.partialString.charCodeAt(i));
+            }
+            this.partialString = null;
+        } else {
+            this.partialString += instruction;
+        }
+    } else if (instruction == '"') {
+        this.partialString = "";
+    } else if ("0" <= instruction && instruction <= "9") {
+        //!console.log("in number");
+        if (this.partialNumber !== null) {
+            this.partialNumber = this.partialNumber + instruction;  // NB: concatenation!
+        } else {
+            this.partialNumber = instruction;
+        }
+        next = this.code.charAt((this.ip + 1) % this.length);
+        if (next < "0" || "9" < next) {
+            // Next instruction is non-numeric, so end number and push it
+            this.currStack.push(+this.partialNumber);
+            this.partialNumber = null;
+        }
+    } else if ("a" <= instruction && instruction <= "f") {
+        // a-f push numbers 10 through 15
+        var value = instruction.charCodeAt(0) - 87;
+        this.currStack.push(value);
+    } else if (instruction == "$") {
+        // Toggle the current stack
+        if (this.currStack === this.ownStack) {
+            this.currStack = this.program.sharedStack;
+        } else {
+            this.currStack = this.ownStack;
+        }
+    } else if (instruction == "s") {
+        this.currStack = this.ownStack;
+    } else if (instruction == "S") {
+        this.currStack = this.program.sharedStack;
+    } else if (instruction == "l") {
+        this.currStack.push(this.ownStack.length);
+    } else if (instruction == "L") {
+        this.currStack.push(this.program.sharedStack.length);
+    } else if (instruction == ".") {
+        var item = this.currStack.pop();
+        this.currStack.push(item);
+        this.currStack.push(item);
+    } else if (instruction == "m") {
+        var item = this.ownStack.pop();
+        this.program.sharedStack.push(item);
+    } else if (instruction == "M") {
+        var item = this.program.sharedStack.pop();
+        this.ownStack.push(item);
+    } else if (instruction == "y") {
+        var item = this.ownStack.top();
+        this.program.sharedStack.push(item);
+    } else if (instruction == "Y") {
+        var item = this.program.sharedStack.top();
+        this.ownStack.push(item);
+    } else if (instruction == "\\") {
+        var top = this.currStack.pop();
+        var next = this.currStack.pop()
+        this.currStack.push(top);
+        this.currStack.push(next);
+    } else if (instruction == "@") {
+        var c = this.currStack.pop();
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(c);
+        this.currStack.push(a);
+        this.currStack.push(b);
+    } else if (instruction == ";") {
+        this.currStack.pop();
+    } else if (instruction == "+") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(a + b);
+    } else if (instruction == "-") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(a - b);
+    } else if (instruction == "*") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(a * b);
+    } else if (instruction == "/") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(a / b);
+    } else if (instruction == "%") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(a % b);
+    } else if (instruction == "_") {
+        this.currStack.push(-this.currStack.pop());
+    } else if (instruction == "I") {
+        var value = this.currStack.pop();
+        if (value < 0) {
+            this.currStack.push(Math.ceil(value));
+        } else {
+            this.currStack.push(Math.floor(value));
+        }
+    } else if (instruction == ">") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(+(a > b));
+    } else if (instruction == "<") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(+(a < b));
+    } else if (instruction == "=") {
+        var b = this.currStack.pop();
+        var a = this.currStack.pop();
+        this.currStack.push(+(a == b));
+    } else if (instruction == "!") {
+        this.currStack.push(+ !this.currStack.pop());
+    } else if (instruction == "?") {
+        this.currStack.push(Math.random());
+    } else if (instruction == "n") {
+        output = "" + this.currStack.pop();
+    } else if (instruction == "o") {
+        output = String.fromCharCode(this.currStack.pop());
+    } else if (instruction == "r") {
+        var input = this.program.io.getNumber();
+        this.currStack.push(input);
+    } else if (instruction == "i") {
+        var input = this.program.io.getChar();
+        this.currStack.push(input);
+    } else if (instruction == "(") {
+        this.length -= Math.floor(this.currStack.pop());
+        this.length = Math.max(this.length, 0);
+    } else if (instruction == ")") {
+        this.length += Math.floor(this.currStack.pop());
+        this.length = Math.min(this.length, this.code.length);
+    } else if (instruction == "w") {
+        this.wait = this.currStack.pop();
+    } else {
+        console.log("Hit else case");
+    }
+    if (this.ip >= this.length) {
+        // We've swallowed the IP, so this snake dies
+        this.alive = false;
+        this.program.snakesLiving--;
+    } else {
+        // Increment IP and loop if appropriate
+        this.ip = (this.ip + 1) % this.length;
+    }
+    return output;
+}
+
+// Define Program class
+function Program(source, speed, io) {
+    this.sharedStack = new Stack();
+    this.snakes = source.split(/\r?\n/).map(function(snakeCode) {
+        var snake = new Snake(snakeCode);
+        snake.program = this;
+        snake.sharedStack = this.sharedStack;
+        return snake;
+    }.bind(this));
+    this.snakesLiving = this.snakes.length;
+    this.io = io;
+    this.speed = speed || 10;
+    this.halting = false;
+}
+
+Program.prototype.run = function() {
+    if (this.snakesLiving) {
+        this.step();
+        this.timeout = window.setTimeout(this.run.bind(this), 1000 / this.speed);
+    }
+}
+
+Program.prototype.step = function() {
+    for (var s = 0; s < this.snakes.length; s++) {
+        var output = this.snakes[s].step();
+        if (output) {
+            this.io.print(output);
+        }
+    }
+}
+
+Program.prototype.halt = function() {
+    window.clearTimeout(this.timeout);
+}
+
+var ioFunctions = {print:     function (item) {
+                                  var stdout = document.getElementById('stdout');
+                                  stdout.value += "" + item;
+                              },
+                   getChar:   function () {
+                                  if (inputData) {
+                                      var inputChar = inputData[0];
+                                      inputData = inputData.slice(1);
+                                      return inputChar.charCodeAt(0);
+                                  } else {
+                                      return -1;
+                                  }
+                              },
+                   getNumber: function () {
+                                  while (inputData && (inputData[0] < "0" || "9" < inputData[0])) {
+                                      inputData = inputData.slice(1);
+                                  }
+                                  if (inputData) {
+                                      var inputNumber = inputData.match(/\d+/)[0];
+                                      inputData = inputData.slice(inputNumber.length);
+                                      return +inputNumber;
+                                  } else {
+                                      return -1;
+                                  }
+                              }};
+
+var program = null;
+var inputData = null;
+
+function resetProgram() {
+    var stdout = document.getElementById('stdout');
+    stdout.value = null;
+    if (program !== null) {
+        program.halt();
+    }
+    program = null;
+    inputData = null;
+}
+
+function initProgram() {
+    var source = document.getElementById('source'),
+        stepsPerSecond = document.getElementById('steps-per-second'),
+        stdin = document.getElementById('stdin');
+    program = new Program(source.value, +stepsPerSecond.innerHTML, ioFunctions);
+    inputData = stdin.value;
+}
+
+function runBtnClick() {
+    if (program === null || program.snakesLiving == 0) {
+        resetProgram();
+        initProgram();
+        program.run();
+    } else {
+        program.halt();
+        var stepsPerSecond = document.getElementById('steps-per-second');
+        program.speed = +stepsPerSecond.innerHTML;
+        window.setTimeout(program.run.bind(program), 1000 / program.speed);
+    }
+}
+
+function stepBtnClick() {
+    if (program === null) {
+        initProgram();
+        program.step();
+    } else {
+        program.halt();
+        window.setTimeout(program.step.bind(program), 1000 / program.speed);
+    }
+}
