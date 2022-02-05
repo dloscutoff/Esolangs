bc43c2818450cb1407658cc363d1eceb91bb9f84
David Loscutoff <dloscutoff@gmail.com>
2022-02-05 13:12:46 -0600

    Added highlighting for parenthesis groups. - Highlight both the hovered paren and the matching paren - Highlight the contents between the parens in a lighter color

--- tinylisp/highlight/highlight.js
+++ tinylisp/highlight/highlight.js
@@ -4,7 +4,7 @@ window.onload = function() {
     rawCode.oninput = function() {
         var formattedCode = document.getElementById("formatted-code");
         // Highlight the raw code and put into formattedCode
-        formattedCode.innerHTML = highlightProgram(this.value);
+        formattedCode.innerHTML = formatProgram(this.value);
         // If the raw code ends with a blank line, we have to add an
         // extra newline at the end of formattedCode to get the last
         // blank line to show up
@@ -19,15 +19,22 @@ window.onload = function() {
     rawCode.focus();
 }
 
-function highlightProgram(code) {
+function formatProgram(code) {
+    var bracketId = 0;
     if (isMultiline(code)) {
         // In multiline mode, treat the whole program as a single unit
         // (only infer parentheses at the end)
-        return highlight(code);
+        return formatSection(code, bracketId)[0];
     } else {
         // In single-line mode, treat each line as a single unit
         // (infer parentheses at the end of each line)
-        return code.split("\n").map(highlight).join("\n");
+        var formattedSection;
+        var formattedSections = [];
+        for (const line of code.split("\n")) {
+            [formattedSection, bracketId] = formatSection(line, bracketId);
+            formattedSections.push(formattedSection);
+        }
+        return formattedSections.join("\n");
     }
 }
 
@@ -39,75 +46,75 @@ function isMultiline(code) {
     // parentheses inferred only at the end. If any line in the code
     // contains more closing parens than opening parens, the code is
     // assumed to be in multiline form; otherwise, it's single-line
-    return code.split("\n").some(line => [...line.matchAll(/\)/g)].length > [...line.matchAll(/\(/g)].length);
+    return code.split("\n").some(line => line.split("(").length > line.split("(").length);
 }
 
-function highlight(code) {
+function formatSection(code, bracketId) {
     var tokens = tokenize(code);
-    var highlightedCode = "";
+    var formattedCode = "";
+    var parsedExpr;
     while (tokens.length > 0) {
-        highlightedCode += parseExpr(tokens, 0)[0];
+        [parsedExpr, bracketId] = parseExpr(tokens, bracketId);
+        formattedCode += parsedExpr;
     }
-    return highlightedCode;
+    return [formattedCode, bracketId];
 }
 
 function tokenize(code) {
     return [...code.matchAll(/[()]|\s|[^()\s]+/g)].map(m => m[0]);
 }
 
-function parseExpr(tokens, parenIndex) {
+function parseExpr(tokens, bracketId) {
     var formattedExpr = "";
     if (tokens.length > 0) {
         var token = tokens.shift();
         if (token === "(") {
             // Parse the whole parenthesized expression recursively
-            var currentParenIndex = parenIndex;
-            formattedExpr += highlightParen(token, currentParenIndex);
-            formattedExpr += `<div id="grp-${currentParenIndex}" class="bracket-contents">`;
-            parenIndex++;
+            var currentBracketId = bracketId;
+            formattedExpr += formatBracket(token, currentBracketId);
+            formattedExpr += `<div id="grp-${currentBracketId}" class="bracket-contents">`;
+            bracketId++;
             while (tokens.length > 0 && tokens[0] !== ")") {
                 var subexpr;
-                [subexpr, parenIndex] = parseExpr(tokens, parenIndex);
+                [subexpr, bracketId] = parseExpr(tokens, bracketId);
                 formattedExpr += subexpr;
             }
             formattedExpr += `</div>`;
             if (tokens.length > 0) {
-                formattedExpr += highlightParen(tokens.shift(), currentParenIndex);
+                formattedExpr += formatBracket(tokens.shift(), currentBracketId);
             } else {
-                formattedExpr += highlightParen(")", currentParenIndex, true);
+                formattedExpr += formatBracket(")", currentBracketId, true);
             }
         } else if (token === ")") {
             // If execution ever gets here, we've got an unmatched close-paren
             formattedExpr = `<span class="unmatched-bracket" title="Unmatched parenthesis">${token}</span>`;
         } else {
             // Any token that's not a parenthesis is an atom
-            formattedExpr = highlightToken(token);
+            formattedExpr = formatToken(token);
         }
     }
-    return [formattedExpr, parenIndex];
+    return [formattedExpr, bracketId];
 }
 
-function highlightParen(paren, parenIndex, isImplied) {
+function formatBracket(bracket, bracketId, isImplied) {
     var mouseover = "";
     var cssClass = "bracket";
     if (isImplied) {
         cssClass += " implied";
         mouseover = `title="Implied parenthesis"`;
     }
-    return `<span id="${paren}-${parenIndex}" class="${cssClass}" ${mouseover}>${paren}</span>`;
+    return `<span id="${bracket}-${bracketId}" class="${cssClass}" ${mouseover} onmouseover="bracketMouseOver.bind(this)()" onmouseout="bracketMouseOut.bind(this)()">${bracket}</span>`;
 }
 
-function highlightToken(token) {
+function formatToken(token) {
     var tokenType, hoverText;
     if (/^\s+$/.test(token)) {
+        // Whitespace doesn't need to be highlighted at all
         return token;
     } else {
         if (/^\d+$/.test(token)) {
             tokenType = "number";
             hoverText = "Integer literal";
-        } else if (token === "(" || token === ")") {
-            tokenType = "bracket";
-            hoverText = null;
         } else if (/^(?:[acdehilqstv]|string|chars|disp|type|load|comment)$/.test(token)) {
             tokenType = "builtin";
             hoverText = "Builtin";
@@ -131,4 +138,22 @@ function highlightToken(token) {
 
 function htmlEscape(token) {
     return token.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll('"', "&quot;");
+}
+
+function bracketMouseOver() {
+    var [bracketType, bracketNumber] = this.id.split("-");
+    var otherBracketType = bracketType === "(" ? ")" : "(";
+    var otherBracket = document.getElementById(`${otherBracketType}-${bracketNumber}`);
+    var contents = document.getElementById(`grp-${bracketNumber}`);
+    otherBracket.classList.add("primary-highlight");
+    contents.classList.add("secondary-highlight");
+}
+
+function bracketMouseOut() {
+    var [bracketType, bracketNumber] = this.id.split("-");
+    var otherBracketType = bracketType === "(" ? ")" : "(";
+    var otherBracket = document.getElementById(`${otherBracketType}-${bracketNumber}`);
+    var contents = document.getElementById(`grp-${bracketNumber}`);
+    otherBracket.classList.remove("primary-highlight");
+    contents.classList.remove("secondary-highlight");
 }
